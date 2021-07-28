9189c59e30b0ec5bf7cddba44fa46e498ac0645a
Collin-5 <collinsotieno380@gmail.com>
2021-07-28 09:07:37 +0300

    js counter

---
+++ counter/index.js
@@ -0,0 +1,34 @@
+//initialize count
+let count = 0;
+
+//get btn and value
+const btns = document.querySelectorAll(".btn");
+const value = document.querySelector("#value");
+
+btns.forEach(function (btn){
+    btn.addEventListener('click', function(e){
+        const obje = e.currentTarget.classList;
+        if (obje.contains("increase")){
+            count++;
+        }
+        
+        else if (obje.contains("decrease")){
+            count--;
+        }
+        else{
+            count = 0;
+        }
+
+        if (count > 0) {
+            value.style.color = "green";
+        }
+        else if (count < 0) {
+            value.style.color = "red";
+        }
+        else {
+            value.style.color = "black";
+        }
+        
+        value.textContent = count;
+    });
+});
